c84224bfb3a2315b1a2076aebf1388f9779cd698
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-12 06:55:50 +0530

    Create greatestof3nos.py

---
+++ greatestof3nos.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first Number: "))
+b=int(input("Enter the second Number: "))
+c=int(input("Enter the thrid Number: "))
+if a>b and a>c:
+    print(a," is the largest Number.")
+elif b>c:
+    print(b," is the largest Number.")
+else:
+    print(c," is the largest Number.")
