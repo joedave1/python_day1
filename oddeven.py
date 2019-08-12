cbb2a1569a79e761903341aab73f9f0948f48e9e
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-12 06:59:57 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,5 @@
+n=int(input("User Enter the Number to check whether the number is odd or even :"))
+if n%2==0:
+    print("The entered number is even.")
+else:
+    print("The entered number is odd.")
