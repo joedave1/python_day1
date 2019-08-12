d1ef0b267f3065155c334f9e7bed779b344799d5
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-12 07:23:59 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,13 @@
+letter=input("Enter a alphabet:")
+if letter=='a':
+    print("The given letter is vowel.")
+elif letter=='e':
+    print("The given letter is vowel.")
+elif letter=='i':    
+    print("The given letter is vowel.")
+elif letter=='o':
+    print("The given letter is vowel.")
+elif letter=='u':
+    print("The given letter is vowel.")
+else:
+    print("The given letter is not a vowel.")
