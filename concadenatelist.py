97715771c0fb4f30471fb8fec88dc27411dd8e79
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-12 06:49:29 +0530

    Create concadenatelist.py

---
+++ concadenatelist.py
@@ -0,0 +1,6 @@
+def Concatenate_list_element(list1):
+    result=""
+    for elements in list1:
+        result=result+str(elements)
+    return result
+print(Concatenate_list_element([2,5,4,3,6,7]))
