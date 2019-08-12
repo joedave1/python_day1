b611560cdfa072c7e60a7d241233c35b9953aaaa
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-12 06:48:07 +0530

    Create copystring.py

---
+++ copystring.py
@@ -0,0 +1,8 @@
+def copy_string(str,n):
+    result=""
+    for i in range(n):
+        result+=str
+    return result
+print(copy_string("abc",3))
+print(copy_string(".py",2))
+print(copy_string(".test",4))
